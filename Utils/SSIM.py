a5e5d142aa73a0f4b066a3d9dc46a0097b20b905
Parker Hicks <65268541+phicks22@users.noreply.github.com>
2022-05-20 18:58:35 -0700

    Update SSIM.py

--- Utils/SSIM.py
+++ Utils/SSIM.py
@@ -3,16 +3,31 @@ from math import exp
 import torch
 import torch.nn.functional as F
 
-def gaussian(window_size, sigma):
-    gauss = torch.Tensor([exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
+def _toimg(mat):
+    m = torch.tensor(mat)
+    # convert to float and add channel dimension
+    return m.float()
+
+def _tohic(mat):
+    mat.squeeze_()
+    return mat.numpy()
+
+def gaussian(width, sigma):
+    gauss = torch.Tensor([exp(-(x-width//2)**2 / float(2 * sigma**2)) for x in range(width)])
     return gauss / gauss.sum()
 
-def create_window(window_size, channel):
-    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
+def create_window(window_size, channel, sigma=3):
+    _1D_window = gaussian(window_size, sigma).unsqueeze(1)
     _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
     window = _2D_window.expand(channel, 1, window_size, window_size).contiguous()
     return window
 
+def gaussian_filter(img, width, sigma=3):
+    img = _toimg(img).unsqueeze(0)
+    _, channel, _, _ = img.size()
+    window = create_window(width, channel, sigma)
+    mu1 = F.conv2d(img, window, padding=width // 2, groups=channel)
+    return _tohic(mu1)
 
 def _ssim(img1, img2, window, window_size, channel, size_average=True):
     mu1 = F.conv2d(img1, window, padding=window_size // 2, groups=channel)
@@ -37,38 +52,9 @@ def _ssim(img1, img2, window, window_size, channel, size_average=True):
         return ssim_map.mean(1).mean(1).mean(1)
 
 
-class SSIM(torch.nn.Module):
-    def __init__(self, window_size=11, size_average=True):
-        super(SSIM, self).__init__()
-        self.window_size = window_size
-        self.size_average = size_average
-        self.channel = 1
-        self.window = create_window(window_size, self.channel)
-
-    def forward(self, img1, img2):
-        (_, channel, _, _) = img1.size()
-
-        if channel == self.channel and self.window.data.type() == img1.data.type():
-            window = self.window
-        else:
-            window = create_window(self.window_size, channel)
-
-            if img1.is_cuda:
-                window = window.cuda(img1.get_device())
-            window = window.type_as(img1)
-
-            self.window = window
-            self.channel = channel
-
-        return _ssim(img1, img2, window, self.window_size, channel, self.size_average)
-
-
 def ssim(img1, img2, window_size=11, size_average=True):
-    (_, channel, _, _) = img1.size()
+    channel = img1.size()[1]
     window = create_window(window_size, channel)
-
-    if img1.is_cuda:
-        window = window.cuda(img1.get_device())
     window = window.type_as(img1)
 
     return _ssim(img1, img2, window, window_size, channel, size_average)
