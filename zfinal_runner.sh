b94500ca911fc7e9dea01227b651b21383645fc7
Parker Hicks <65268541+phicks22@users.noreply.github.com>
2022-07-19 23:40:33 -0600

    Update zfinal_runner.sh

--- zfinal_runner.sh
+++ zfinal_runner.sh
@@ -13,7 +13,7 @@
 # python Generate.py -hr 10kb -lr 40kb -chunk 40 -stride 40 -bound 201 -scale 1 -c GM12878
 
 # Predicting data
-#python data_predict.py -lr 40kb -ckpt path/to/ckpt/hicarn.pytorch -c GM12878
+# python 40x40_Predict.py -m HiCARN_1 -lr 40kb -ckpt root_dir/checkpoints/weights_file.pytorch -f hicarn_10kb40kb_c40_s40_b201_nonpool_human_GM12878_test.npz -c GM12878_HiCARN_1
 
 # Training the model
-#python HiCARN_1_Train.py
+# python HiCARN_1_Train.py
